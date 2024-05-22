bfec9b86d40e0ca82f474e59688d72d5413f7750
Jessie Yu <xkyu815@gmail.com>
2024-05-21 22:56:57 -0400

    adding event listener to post entered patient information

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
     const patientContainer = document.querySelector('#patient-container')
     const patientURL = 'http://localhost:3000/patient'
+    const patientForm = document.querySelector('#patient-form')
+    const idInput = patientForm.querySelector('#id').value
+    const firstNameInput = patientForm.querySelector('#firstName').value
+    const lastNameInput = patientForm.querySelector('#lastName').value
+    const dobInput = patientForm.querySelector('#dob').value
+    const genderInput = patientForm.querySelector('#gender').value
+    const admissionDateInput = patientForm.querySelector('#admissionDate').value
+    const dischargeDateInput = patientForm.querySelector('#dischargeDate').value
+    const currentBedInput = patientForm.querySelector('#currentBed').value
+
+
 
     fetch(`${patientURL}`)
     .then( response => response.json() )  
@@ -19,4 +30,27 @@ document.addEventListener('DOMContentLoaded', function() {
         <button data-id="${patient.id}" id="transfer-${patient.id}" data-action="Transfer">Transfer</button>
       </div>`
     })) // end of book fetch
+
+    patientForm.addEventListener('submit', (e) => {
+        e.preventDefault()
+        console.log(e.target)
+        fetch(`${patientURL}`, {
+            method: 'POST',
+            body: JSON.stringify({
+              id: idInput,
+              firstName: firstNameInput,
+              lastName: lastNameInput,
+              dob: dobInput,
+              gender: genderInput,
+              admissionDate: admissionDateInput,
+              dischargeDate: dischargeDateInput,
+              currentBed: currentBedInput
+            }),
+            headers: {
+              'Content-Type': 'application/json'
+            }
+   })
+
+   
+    })
  })
