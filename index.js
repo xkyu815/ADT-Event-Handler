d801d2ca1234ab825cf13dd12ffe8f8afe1cb253
Jessie Yu <xkyu815@gmail.com>
2024-05-22 02:02:07 -0400

    find the patient through id in all patients

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ document.addEventListener('DOMContentLoaded', function() {
     const patientContainer = document.querySelector('#patient-container')
     const patientURL = 'http://localhost:3000/patient'
     const patientForm = document.querySelector('#patient-form')
+    let allPatients = []
 
 
     fetch(`${patientURL}`)
     .then( response => response.json() )  
     .then( patientData => patientData.forEach(function(patient) {
+      allPatients = patientData
       patientContainer.innerHTML += `
       <div id=${patient.id}>
         <h2>${patient.firstName}</h2>
@@ -16,9 +18,10 @@ document.addEventListener('DOMContentLoaded', function() {
         <h4>Admission Date: ${patient.admissionDate}</h4>
         <h4>Discharge Date: ${patient.dischargeDate}</h4>
         <h4>Current Bed: ${patient.currentBed}</h4>
-        <button data-id="${patient.id}" id="admission-${patient.id}" data-action="Admission">Admission</button>
-        <button data-id="${patient.id}" id="discharge-${patient.id}" data-action="Discharge">Discharge</button>
-        <button data-id="${patient.id}" id="transfer-${patient.id}" data-action="Transfer">Transfer</button>
+        <button data-id="${patient.id}" id="admission-${patient.id}" data-action="admission">Admission</button>
+        <button data-id="${patient.id}" id="discharge-${patient.id}" data-action="discharge">Discharge</button>
+        <button data-id="${patient.id}" id="transfer-${patient.id}" data-action="transfer">Transfer</button>
+        <button data-id="${patient.id}" id="view-${patient.id}" data-action="view">View</button>
       </div>`
     }
     
@@ -34,8 +37,6 @@ document.addEventListener('DOMContentLoaded', function() {
     const admissionDateInput = patientForm.querySelector('#admissionDate').value
     const dischargeDateInput = patientForm.querySelector('#dischargeDate').value
     const currentBedInput = patientForm.querySelector('#currentBed').value
-
-    
 
     fetch(`${patientURL}`, {
         method: 'POST',
@@ -63,10 +64,20 @@ document.addEventListener('DOMContentLoaded', function() {
         <h4>Admission Date: ${patient.admissionDate}</h4>
         <h4>Discharge Date: ${patient.dischargeDate}</h4>
         <h4>Current Bed: ${patient.currentBed}</h4>
-        <button data-id="${patient.id}" id="admission-${patient.id}" data-action="Admission">Admission</button>
-        <button data-id="${patient.id}" id="discharge-${patient.id}" data-action="Discharge">Discharge</button>
-        <button data-id="${patient.id}" id="transfer-${patient.id}" data-action="Transfer">Transfer</button>
       </div>`
     })
-   })  
+   }) 
+   
+   patientContainer.addEventListener('click', (e) => {
+    if (e.target.dataset.action === 'admission') {
+        const patientData = allPatients.find((patient) => {
+            return patient.id == e.target.dataset.id
+          })
+          console.log(patientData)
+    } else if (e.target.dataset.action === 'discharge') {
+      console.log('you pressed discharge')
+    } else if (e.target.dataset.action === 'transfer') {
+        console.log('you pressed transfer')
+      }
+  }) // end of eventListener for editing and deleting a patient
 })
